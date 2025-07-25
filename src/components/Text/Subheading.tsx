d8c2b46bbd59f1126b8961b82e40d6263425518b
fsanchez1397 <felipesanchez10@protonmail.com>
2025-07-25 02:48:07 -0400

    feat: Landing page template

--- src/components/Text/Subheading.tsx
+++ src/components/Text/Subheading.tsx
@@ -2,7 +2,7 @@ interface SubheadingProps {
   text: string;
 }
 function Subheading({ text }: SubheadingProps) {
-  return <div>{text}</div>;
+  return <h3 className="text-2xl">{text}</h3>;
 }
 
 export default Subheading;
